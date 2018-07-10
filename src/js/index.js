4ec2e8b70e86f8914e5e5ed462b653256e33805e
Nemanja Ristic <rista.pirot@gmail.com>
2018-07-10 15:33:23 +0200

    Updates were made

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 require('../css/main.scss')
+
+var elements = document.getElementsByClassName('sidebar__text')
+var icons = document.getElementsByClassName('sidebar__icon')
+
+document.getElementsByClassName('toolbox__menu-icon')[0].addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('toggle')
+    document.querySelector('.main-content').classList.toggle('toggle-main')
+    for(var el of elements) {
+        el.classList.toggle('toggle-text')
+    }
+
+    for(var icon of icons) {
+        icon.classList.toggle('toggle-icon')
+    }
+})
+
+document.querySelector('.fa-bell').addEventListener('click', function() {
+    document.querySelector('.toolbox__list').classList.toggle('show')
+})
